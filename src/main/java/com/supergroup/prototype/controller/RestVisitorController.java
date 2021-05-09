c850646d566950927546584904a04f8f0c5392dd
kootasu <kootasu@hotmail.com>
2021-05-09 12:04:53 +0200

    Testdata virker (bortset fra dateAndTime), kan ses i H2

--- src/main/java/com/supergroup/prototype/controller/RestVisitorController.java
+++ src/main/java/com/supergroup/prototype/controller/RestVisitorController.java
@@ -1,4 +1,21 @@
 package com.supergroup.prototype.controller;
 
+import com.supergroup.prototype.service.VisitorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RestVisitorController {
+
+    @Autowired
+    VisitorService visitorService;
+
+    @GetMapping("/visitors")
+    public String mods(Model model){
+        model.addAttribute("visitors", visitorService.findAll());
+        return "visitors";
+    }
+
 }
--- src/main/java/com/supergroup/prototype/service/VisitorService.java
+++ src/main/java/com/supergroup/prototype/service/VisitorService.java
@@ -3,10 +3,12 @@ package com.supergroup.prototype.service;
 import com.supergroup.prototype.model.Visitor;
 import com.supergroup.prototype.repository.VisitorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
 public class VisitorService {
 
     @Autowired
