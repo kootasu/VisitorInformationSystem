d4a4d27ead60cc97b24a6c6e848f422952f586c0
kootasu <kootasu@hotmail.com>
2021-05-21 14:07:22 +0200

    More styling (bigger buttons, bigger icons etc.)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO visitor (timestamp, gender, language, age_Group, group_Size, in_Large_Group) VALUES (current_timestamp ,'Kvinde', 'Dansk', '55+', 2, 0);
-INSERT INTO visitor (timestamp, gender, language, age_Group, group_Size, in_Large_Group) VALUES (current_timestamp ,'Mand', 'Engelsk', '55+', 2, 0);
+INSERT INTO visitor (timestamp, gender, language, age_Group, group_Size, in_Large_Group) VALUES (current_timestamp ,'female', 'Danish', '55+', 2, 0);
+INSERT INTO visitor (timestamp, gender, language, age_Group, group_Size, in_Large_Group) VALUES (current_timestamp ,'male', 'English', '55+', 2, 0);
 
 INSERT INTO user (username, password) VALUES ('username', 'password');
 
