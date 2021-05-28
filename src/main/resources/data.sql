4fff8dbc571bebdb8ca839b5c41ee10a549b1273
kootasu <kootasu@hotmail.com>
2021-05-28 09:29:15 +0200

    Database naming conventions again

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO visitor (timestamp, gender, language, age_Group, group_Size, in_Large_Group) VALUES (current_timestamp ,'female', 'Danish', '55+', 2, 0);
-INSERT INTO visitor (timestamp, gender, language, age_Group, group_Size, in_Large_Group) VALUES (current_timestamp ,'male', 'English', '55+', 2, 0);
+INSERT INTO visitor (timestamp, gender, language, age_Group, group_Size, in_Large_Group) VALUES (current_timestamp ,'kvinde', 'dansk', '55+', 2, 0);
+INSERT INTO visitor (timestamp, gender, language, age_Group, group_Size, in_Large_Group) VALUES (current_timestamp ,'mand', 'engelsk', '55+', 2, 0);
 
 create table authorities (
                              username varchar(50) not null,
